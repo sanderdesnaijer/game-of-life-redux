4aea81cf717228d92a4a2de6caf9a6c904633c64
Sander de Snaijer <sander@labela.nl>
2018-03-18 20:11:31 +0100

    changed fixtures

--- src/constants/fixture.js
+++ src/constants/fixture.js
@@ -1,3 +1,22 @@
 // R-pentomino
 // https://www.theguardian.com/science/alexs-adventures-in-numberland/2014/dec/15/the-game-of-life-a-beginners-guide
-export default [[30, 23], [30, 24], [31, 22], [31, 23], [32, 23]];
+export default [
+  [12, 13],
+  [12, 14],
+  [13, 15],
+  [14, 16],
+  [15, 16],
+  [16, 16],
+  [17, 15],
+  [18, 13],
+  [18, 14],
+  [30, 40],
+  [30, 41],
+  [31, 39],
+  [32, 38],
+  [33, 38],
+  [34, 38],
+  [35, 39],
+  [36, 40],
+  [36, 41],
+];
